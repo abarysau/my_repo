e16c139c1380c105d295dc7013a5f6814dcc0c02
abarysau <a_barysau@iba.by>
2022-02-18 14:47:38 +0300

    My first commit

---
+++ python_test.py
@@ -0,0 +1,6 @@
+# This is a dumy file to test GitHub
+
+print('\n----------------\nHOME WORK 2\n----------------\n')
+
+print('\n----------------\nHOME WORK COMPLETED\n----------------\n')
+dumy_str = input('Enter anything to exit')
